39bb5cbc53b998eaff81927f78426b1c5e238d82
riyasri1166 <riyasri1166@gmail.com>
2023-09-27 14:43:56 +0530

    message is last

---
+++ src/org/mass/Test.java
@@ -0,0 +1,10 @@
+package org.mass;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Test1,Jeevi oru Korangu");
+		System.out.println("akka oru teachermadam");
+		System.out.println("priya oru charming girl");
+	}
+}
